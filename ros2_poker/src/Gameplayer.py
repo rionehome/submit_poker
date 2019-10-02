476bb0cdb4c69d2f9e4658cae256aba013c883a0
tatsuya_ego <ts2000kasa@gmail.com>
2019-10-02 19:37:15 +0900

    ミスりました申し訳ナッシングトゥーマッチ

--- ros2_poker/src/Gameplayer.py
+++
@@ -1,9 +0,0 @@
-# 人数の送信データ
-class peoplenumber(Node):
-    def __init__(self):
-        super().__init__('pnumber')
-        self.publisher = self.create_publisher(string, 'chatter')
-if __name__ == '__main__':
-#pythonクライアントライブラリの初期化  
-    rclpy.init(args = args)
-#publisherの作成
